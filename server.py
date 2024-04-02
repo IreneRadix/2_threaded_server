c07cc9fbae33b6475c1f9bb74859f88e02441796
IreneRadix <irinagoncarova094@gmail.com>
2024-04-02 14:41:09 +0300

    commit

--- server.py
+++ server.py
@@ -1,20 +1,43 @@
 import socket
+from threading import Thread
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+class Server:
+	def __init__(self, port=200):
+		self.port = port
+		self.host = input('input host address: ')
+		self.sock = None
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	def __init_server(self):
+		while True:
+			try:
+				sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				sock.bind((self.host, self.port))
+				print(f"Server is listening on port {self.port}")
+				sock.listen(1)
+				return sock
+			except IOError:
+				print(f"Port {self.port} is already in use, trying the next one...")
+				self.port += 1
 
-print(msg)
+	@staticmethod
+	def communicate_user(client_socket, address):
+		print("Client accepted")
+		print("Client address:", address[0])
+		print("Client port:", address[1])
+		while True:
+			while True:
+				data = client_socket.recv(1024)
+				if data.decode() == 'exit':
+					break
+				client_socket.send(data.upper())
+			client_socket.close()
 
-conn.close()
+	def main(self):
+		self.sock = self.__init_server()
+		client_socket, address = self.sock.accept()
+		Thread(target=self.communicate_user, args=(client_socket, address)).start()
+
+
+server = Server()
+server.main()
